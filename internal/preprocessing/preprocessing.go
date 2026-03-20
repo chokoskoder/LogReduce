544ca2916c4409e3849ee0bea3e7c67f967f2f6d
chokoskoder <chokoskoder@gmail.com>
2026-03-21 01:29:22 +0530

    finished with the preprocessing pipeline

--- internal/preprocessing/preprocessing.go
+++ internal/preprocessing/preprocessing.go
@@ -1,7 +1,10 @@
 package preprocessing
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/linkedin/goavro/v2"
 )
@@ -10,12 +13,7 @@ import (
 The aim of this code is to convert the json files into physical splits of the avro schema we will define
 */
 
-func preprocessing() {
-	codec, err := goavro.NewCodec(LogEntrySchema)
-	if err != nil {
-		fmt.Println(err)
-	}
-	
+func preprocessing(inputPath string , outputPaths []string) error {
 	/*
 	so the flow will be :
 	1)	read a json line
@@ -24,4 +22,63 @@ func preprocessing() {
 	4)	use ocf writer in round robin format to add to necessary file and et voila
 	*/
 
+
+	//start by reading the json file
+	file, err := os.Open("app_logs.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	numSplits := 5
+	files := make([]*os.File , numSplits)
+
+	for i := 0 ; i < numSplits ; i++ {
+		f,err := os.Create(fmt.Sprintf("split-%d.avro" , i))
+		if err!= nil {
+			return err
+		}
+		files[i] = f
+	}
+
+	defer func() {
+		for _ , f := range files {
+			f.Close()
+		}
+	}()
+
+	writers := make([]*goavro.OCFWriter , numSplits)
+	for i := 0; i<numSplits ; i++{
+		w , err := goavro.NewOCFWriter(goavro.OCFConfig{
+			W : files[i],
+			Schema: LogEntrySchema,
+		})
+		if err != nil {
+			return err
+		}
+		writers[i] = w
+	}
+	counter := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var record map[string]interface{}
+		if err := json.Unmarshal([]byte(line) , &record); err != nil{
+			continue //skip the malformed lines
+		}
+
+		//fix the type issues:
+		record["status"] = int32(record["status"].(float64))
+    	record["latency_ms"] = int32(record["latency_ms"].(float64))
+    	record["user_id"] = int32(record["user_id"].(float64))
+
+
+		writers[counter % numSplits].Append([]interface{}{record})
+	    counter++
+
+	}
+
+	return nil
 }
