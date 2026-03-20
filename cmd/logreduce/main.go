fcc614aa5616e1e61b2460ebfa5d8e21e032a973
chokoskoder <chokoskoder@gmail.com>
2026-03-21 04:28:20 +0530

    the preprocessing pipeline works fine

--- cmd/logreduce/main.go
+++ cmd/logreduce/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/chokoskoder/LogReduce/internal/preprocessing"
+)
+
+func main(){
+	numSplits := 5
+	inputPath := "app_logs.json"
+	FilePaths , err := preprocessing.ConvertAndSplit(numSplits, inputPath)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for _,filePaths := range FilePaths {
+			fmt.Println(filePaths)
+		}
+	}
+}
