006cd6faaa73093bcbc26b88aa8ba64c3ae1986a
Siddhant Chopra <chokoskoder@gmail.com>
2026-03-30 11:58:48 +0530

    rebasing commits

--- cmd/logreduce/main.go
+++ cmd/logreduce/main.go
@@ -6,15 +6,17 @@ import (
 	"github.com/chokoskoder/LogReduce/internal/preprocessing"
 )
 
-func main(){
+func main() {
 	numSplits := 5
 	inputPath := "app_logs.json"
-	FilePaths , err := preprocessing.ConvertAndSplit(numSplits, inputPath)
+	FilePaths, err := preprocessing.ConvertAndSplit(numSplits, inputPath)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		for _,filePaths := range FilePaths {
+		for _, filePaths := range FilePaths {
 			fmt.Println(filePaths)
 		}
 	}
+
+	fmt.Println("practincing git rebasing")
 }
